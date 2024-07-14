0f09b28bddedee9282283a9831ceabe8f33ee798
Haifeng Liu <xleo.js@gmail.com>
2024-07-14 06:10:21 +0000

    feat: update redis

--- app/lib/redis.ts
+++ app/lib/redis.ts
@@ -1,12 +1,6 @@
 import Redis from "ioredis";
 
 // 创建一个 Redis 客户端
-const redis = new Redis("redis://:zWe(80)E@47.99.186.111:6379/0");
+const redis = new Redis(process.env.REDIS_URL as string);
 
 export default redis;
-// {
-//   // host: "localhost", // Redis 服务器的主机名或IP地址
-//   // port: 6379, // Redis 服务器的端口号
-//   // 如果需要密码认证，请添加如下行
-//   // password: 'your_redis_password'
-// }
