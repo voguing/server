28313372229bfa3c7efa6ee762bb63d2879ec1a6
Haifeng Liu <xleo.js@gmail.com>
2024-07-14 06:56:00 +0000

    feat: update command

--- app/server/compose.ts
+++ app/server/compose.ts
@@ -6,21 +6,22 @@ import { connect } from "../lib/ssh";
 import os from "os";
 import fs from "fs-extra";
 
+const DOWN_COMMAND = "docker compose -f /root/docker-compose.yml down";
+const UP_COMMAND = "docker compose -f /root/docker-compose.yml up -d";
+
 export const composeStop = async (host: string) => {
   const ssh = await connect(host);
-  await ssh.execCommand("docker compose -f /root/docker-compose.yml down");
+  await ssh.execCommand(DOWN_COMMAND);
 };
 
 export const composeStart = async (host: string) => {
   const ssh = await connect(host);
-  await ssh.execCommand("docker compose -f /root/docker-compose.yml up -d");
+  await ssh.execCommand(UP_COMMAND);
 };
 
 export const composeRestart = async (host: string) => {
   const ssh = await connect(host);
-  await ssh.execCommand(
-    "docker compose -f /root/docker-compose.yml down && docker compose -f /root/docker-compose.yml up -d"
-  );
+  await ssh.execCommand(`${DOWN_COMMAND} && ${UP_COMMAND}`);
 };
 
 export const composeChange = async (host: string, config: string) => {
